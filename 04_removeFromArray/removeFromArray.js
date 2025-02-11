5800190a2172c9908f69d9524fb3f5dd8bfd50a6
Robert Eisenman <robert.t.eisenman@gmail.com>
2025-02-10 23:30:34 -0800

    remove from array test 1

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, argument) {
+    const returnArray = [];
+
+    for (const value of array) {
+        if (value === argument) continue;
+        else returnArray.push(value);
+    }
+
+    return returnArray;
 };
 
 // Do not edit below this line
