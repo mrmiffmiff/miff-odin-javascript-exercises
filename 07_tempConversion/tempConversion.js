3539ef964fc94a65b34660c520a37fb17c681d0b
Robert Eisenman <robert.t.eisenman@gmail.com>
2025-02-11 23:35:46 -0800

    temp conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+function roundToOneDecimalPlace(number) {
+  return Math.round(number * 10) / 10; // this feels so silly but it works
+}
+
+const convertToCelsius = function (fTemp) {
+  return roundToOneDecimalPlace((fTemp - 32) * (5 / 9));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (cTemp) {
+  return roundToOneDecimalPlace(cTemp * (9 / 5) + 32);
 };
 
 // Do not edit below this line
