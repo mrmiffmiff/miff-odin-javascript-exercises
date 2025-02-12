7058cba59fd575637249bcec81ab971d1cdea1bd
Robert Eisenman <robert.t.eisenman@gmail.com>
2025-02-11 23:27:08 -0800

    leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+// given solution uses const booleans to represent these checks but I personally don't see the use in this case
+const leapYears = function (year) {
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            return year % 400 === 0;
+        }
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
