ef3eaf44b1c59a9be4a74f294006bea3183cf9cf
Robert Eisenman <robert.t.eisenman@gmail.com>
2025-02-24 13:06:25 -0800

    fibonacci (probably not how they wanted it but I know recursion)

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function (targetIndex) {
+    let target = parseInt(targetIndex);
+    if (target < 0) return "OOPS";
+    if (target === 0) return 0;
+    if (target === 1 || target === 2) return 1;
+    return fibonacci(target - 2) + fibonacci(target - 1);
 };
 
 // Do not edit below this line
