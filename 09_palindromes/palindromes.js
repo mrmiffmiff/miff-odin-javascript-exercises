a0f513b6e71bdd0d78bfc84f4207356373e0bffb
Robert Eisenman <robert.t.eisenman@gmail.com>
2025-02-24 12:56:40 -0800

    palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let stArr = string.toLowerCase().split('').filter(isAlphaNumeric);
+    return stArr.join('') === stArr.reverse().join('');
 };
+
+function isAlphaNumeric(char) {
+    let alphaString = "0123456789abcdefghijklmnopqrstuvwxyz";
+    return alphaString.includes(char);
+}
 
 // Do not edit below this line
 module.exports = palindromes;
