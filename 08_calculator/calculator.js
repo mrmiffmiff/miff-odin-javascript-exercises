74a75366afa8d7acc650e60e938a4982e0810118
Robert Eisenman <robert.t.eisenman@gmail.com>
2025-02-24 12:27:22 -0800

    calculator add and subtract

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,25 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const sum = function () {
 
 };
 
-const power = function() {
-	
+const multiply = function () {
+
 };
 
-const factorial = function() {
-	
+const power = function () {
+
+};
+
+const factorial = function () {
+
 };
 
 // Do not edit below this line
