91c19624e99f16a93ef9fdd3b6382b5332f9867e
Robert Eisenman <robert.t.eisenman@gmail.com>
2025-02-24 13:31:41 -0800

    find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+function getAge(person) {
+    if (Object.hasOwn(person, "yearOfDeath")) {
+        return person.yearOfDeath - person.yearOfBirth;
+    }
+    else {
+        const currentYear = new Date().getFullYear();
+        return currentYear - person.yearOfBirth;
+    }
+}
 
+const findTheOldest = function (people) {
+    return people.reduce((oldest, current) => {
+        if (getAge(current) > getAge(oldest)) return current;
+        else return oldest;
+    });
 };
 
 // Do not edit below this line
